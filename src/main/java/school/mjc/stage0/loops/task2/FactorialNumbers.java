f0c6a7f8e5225bfc4a587b941cc7d54dfb55eac8
Andreyome <andreyomem@gmail.com>
2022-12-16 03:38:16 +0200

    fix 3

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,11 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-        int number=0;
-        int result=0;
+        int number=1;
+        int result=1;
+        System.out.println(result);
         while (number<=printToInclusive){
             result=result*number++;
             System.out.println(result);
         }
+
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,11 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-        if(power>0){
-            int number = 0;
+        if(power>=0){
+            int number = 1;
             while (number<=power){
                 System.out.println((int)(Math.pow(2,number++)));
             }
+        }
+        else{
+            System.out.println("too much power");
         }
     }
 }
