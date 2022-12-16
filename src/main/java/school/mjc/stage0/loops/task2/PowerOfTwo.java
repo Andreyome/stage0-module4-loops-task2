4d846e5de592cf0b448db07d0320270104ac52d1
Andreyome <andreyomem@gmail.com>
2022-12-16 03:45:13 +0200

    fix 4

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task2;
 public class PowerOfTwo {
     public void printPower(int power) {
         if(power>=0){
-            int number = 1;
+            int number = 0;
             while (number<=power){
                 System.out.println((int)(Math.pow(2,number++)));
             }
