35fc599b734f33fb74ae51f11fe5231a83ef511b
Andreyome <andreyomem@gmail.com>
2022-12-16 03:29:48 +0200

    fix 2

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-        int number=1;
-        int result=1;
+        int number=0;
+        int result=0;
         while (number<=printToInclusive){
             result=result*number++;
             System.out.println(result);
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
-for (int i=1;i<=printToInclusive;i++){
+for (int i=2;i<=printToInclusive;i++){
     int mark=0;
     for (int j=2;j<i;j++){
         if (i%j ==0){
